89afe1ad630224d2685aab757fead3a8eea3112a
Rahul Raj <rahulraz1308@gmail.com>
2024-01-02 16:59:07 +0530

    initial react project setup with webpack

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import SampleImg from "./assets/sample.jpg";
+
+import "./style.scss";
+
+const App = () => {
+  const ss = "";
+  return (
+    <div className="main-container">
+      <h1>Hello react bbb</h1>
+      <img src={SampleImg} width="100" height="100" />
+    </div>
+  );
+};
+
+export default App;
